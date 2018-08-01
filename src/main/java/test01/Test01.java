231634ec2edf33deb9fdb8cd35b78873854be452
xuxy <xuxy@fengyuntec.com>
2018-08-01 15:47:55 +0800

    init

---
+++ src/main/java/test01/Test01.java
@@ -0,0 +1,17 @@
+package test01;
+
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
+
+public class Test01 {
+    public static void main(String[] args){
+        try {
+            Job job = new Job();
+            job.setJarByClass(Test01.class);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
