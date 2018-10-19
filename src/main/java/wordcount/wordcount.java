8a0600ab12130215094e843400258e5e15e50931
徐雪阳 <xuxy@fengyuntec.com>
2018-10-19 17:17:22 +0800

    init

--- src/main/java/wordcount/wordcount.java
+++ src/main/java/wordcount/wordcount.java
@@ -5,7 +5,6 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
